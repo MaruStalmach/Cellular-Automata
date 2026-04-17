75a61ea8d37f142eff22c36e66e5c32cd382808c
jakwojc <wojciechowskicuba@gmail.com>
2026-04-17 21:17:10 +0200

    added back deleted geometry code and fixed random state init

--- libs/Cells.py
+++ libs/Cells.py
@@ -130,7 +130,12 @@ collection of cells, arranged in a 2D or 3D matrix
         if not random:
             self._data = np.full(geometry.size,ZERO_CELL)
         else:
-            self._data = np.full(geometry.size, Cell(cell_keys,random_func=np.random.randint, random_args = {'low':2}))
+            self._data = np.full(geometry.size,ZERO_CELL)
+            self._data = self._data.flatten()
+            for i,_ in enumerate(self._data):
+                self._data[i] = Cell(keys=cell_keys,random=True,random_func=np.random.randint,random_args={'low':2})
+
+            self._data = self._data.reshape(geometry.size)
             
         # add padding
         self._data = np.pad(self._data,((1,1),(1,1),(1,1)),constant_values=ZERO_CELL)
