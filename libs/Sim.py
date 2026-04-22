d8e9381da047b271649472ac450c902f19b0b240
MaruStalmach <marustalmach@student.agh.edu.pl>
2026-04-22 13:39:54 +0200

    update on Sim based on Geometry

--- libs/Sim.py
+++ libs/Sim.py
@@ -22,23 +22,30 @@ CA Simulation engine
         """
         self.rules = rules
         self.geometry = geometry
-        self.mask = np.array([
-            [
-                [1,1,1],
-                [1,1,1],
-                [1,1,1]
-            ],
-            [
-                [1,1,1],
-                [1,0,1],
-                [1,1,1]
-            ],
-            [
-                [1,1,1],
-                [1,1,1],
-                [1,1,1]
-            ],
-        ])
+
+        self.neighbours = geometry.generate_neighbourhood_matrix()
+        self._neighbours_idx = [
+            self.neighbours.indices[
+                self.neighbours.indptr[i]:self.neighbours.indptr[i+1]
+            ] for i in range(geometry.num_cells)]
+    
+        # self.mask = np.array([
+        #     [
+        #         [1,1,1],
+        #         [1,1,1],
+        #         [1,1,1]
+        #     ],
+        #     [
+        #         [1,1,1],
+        #         [1,0,1],
+        #         [1,1,1]
+        #     ],
+        #     [
+        #         [1,1,1],
+        #         [1,1,1],
+        #         [1,1,1]
+        #     ],
+        # ])
 
         # get keys reuired by rules used in the simulation
         keys = []
@@ -72,7 +79,7 @@ CA Simulation engine
         # iterate over all the cells in the state matrix
         #TODO: split among threads
         for cell in range(len(flattened)): 
-            neighbors = self._select_neighbors(self._dim1_to_dim3_coords(cell,self.state.shape))
+            neighbours = self._select_neighbors(cell, flattened)
 
             original_cell = flattened[cell]
             this_cell = Cell(keys=original_cell.keys,random=False)
@@ -81,7 +88,7 @@ CA Simulation engine
 
             
             for rule in self.rules:
-                this_cell = rule.apply(neighbors, this_cell)
+                this_cell = rule.apply(neighbours, this_cell)
             x,y,z = self._dim1_to_dim3_coords(cell,state.shape)
             new_state[x, y, z] = this_cell
         
@@ -92,10 +99,7 @@ CA Simulation engine
         """converts 1D coordinate to 3D coordinate, given the shape of the state matrix"""
         return np.unravel_index(dim1_coord, state_shape)
         
-    def _select_neighbors(self,location):
-    
-        lx,ly,lz = location
-        
-        selection = self.state._data[lx:lx+3,ly:ly+3,lz:lz+3] * self.mask
-
-        return selection.flatten().tolist()
+    def _select_neighbors(self,cell_idx_1d:int, flattened_state: np.ndarray):
+        """select neighbors of a cell given its location in the state matrix"""
+        neighbors_idx = self._neighbours_idx[cell_idx_1d]
+        return [flattened_state[idx] for idx in neighbors_idx]
