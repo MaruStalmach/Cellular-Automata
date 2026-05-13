9ec146f5d206d62edaf744d2976d9ada151f928a
maru <marustalmach@student.agh.edu.pl>
2026-05-13 18:03:35 +0200

    added tests for survival of cells in Life 5677

--- tests/test_game_of_life_3D.py
+++ tests/test_game_of_life_3D.py
@@ -8,6 +8,21 @@ def make_cell(alive: int) -> Cell:
     cell['alive'] = alive
     return cell
 
+def make_neighbour(n_alive: int, total: int = 26):
+    '''helper function for returning a list with n_alive cells out of total'''
+    return [make_cell(1)] * n_alive + [make_cell(0)] * (total - n_alive)
+
+
+def make_gol_4555(geometry: Geometry):
+    '''config for Life 4555'''
+    gol = GameOfLife3D(geometry=geometry)
+    gol.eb = 4
+    gol.eh = 5
+    gol.fb = 5
+    gol.fh = 5
+
+    return gol
+
 # BASIC TESTS
 
 def test_GoL_birth():
@@ -50,4 +65,25 @@ def test_GoL_death():
     assert result['alive'] == 0
     assert type(cell) == Cell
 
-    
+
+# TESTS BASED ON https://content.wolfram.com/sites/13/2018/02/01-3-1.pdf
+
+def test_5677_survival_at_lower_bound():
+    '''alive cells with exacty 5 neiehghbours survives'''
+    geometry = Geometry((2,2,2), axes='xy', periodicity='y')
+    gol = GameOfLife3D(geometry=geometry)
+
+    cell = make_cell(1)
+    result = gol.apply(neighbors=make_neighbour(5), cell=cell)
+
+    assert result['alive'] == 1
+
+def test_5677_survival_at_upper_bound():
+    '''alive cell with exactly 7 neighbours survives'''
+    geometry = Geometry((2,2,2), axes='xy', periodicity='y')
+    gol = GameOfLife3D(geometry=geometry)
+
+    cell = make_cell(1)
+    result = gol.apply(neighbors=make_neighbour(7), cell=cell)
+
+    assert result['alive'] == 1
