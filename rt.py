7261027e4373b8e9a5216c61790108c093bf475d
jakwojc <wojciechowskicuba@gmail.com>
2026-06-02 13:36:34 +0200

    updated rendering with new pipeline

--- rt.py
+++ rt.py
@@ -13,9 +13,11 @@ import glm
 
 
 vertexShaderSource = """
-#version 330 core
+#version 430 core
 
 layout (location=0) in vec3 aPos;
+
+flat out int InstanceID;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -23,20 +25,35 @@ uniform mat4 projection;
 uniform vec3 grid_size;
 
 void main(){
-   gl_Position=projection*view*model*vec4(aPos,1.0); 
+    InstanceID = gl_InstanceID;
+    int x = gl_InstanceID % int(grid_size[0]);
+    int y = gl_InstanceID/int(grid_size[0]);
+    y = y % int(grid_size[1]);
+    int z = gl_InstanceID/(int(grid_size[0])*int(grid_size[1]));
+    int num_cubes = int(grid_size[0]*grid_size[1]*grid_size[2]);
+
+    vec3 bPos = aPos;
+    bPos.x = bPos.x+1.0*float(x);
+    bPos.y = bPos.y+1.0*float(y);
+    bPos.z = bPos.z+1.0*float(z);
+
+    gl_Position=projection*view*model*vec4(bPos,1.0);
 }
 """
 
 fragmentShaderSource = """
-#version 330 core
+#version 430 core
 
 out vec4 FragColor;
 
+flat in int InstanceID; 
+
 uniform float window_width;
 uniform float window_height;
+uniform float[1000] color_data;
 
 void main(){
-   FragColor=vec4(gl_FragCoord.x/window_width, gl_FragCoord.y/window_height, gl_FragCoord.z/100.0, 0.8); 
+   FragColor=vec4(color_data[InstanceID], gl_FragCoord.y/window_height, gl_FragCoord.z/100.0, 0.8); 
 }
 """
 
@@ -45,7 +62,45 @@ void main(){
 WINDOW_WIDTH=1000
 WINDOW_HEIGHT=600
 
-GRID_SIZE = (3,1,1)
+GRID_SIZE = (5,5,2)
+
+def key_callback(window, key, scancode, action, mods):
+    if key == glfw.KEY_A and action == glfw.PRESS:
+        ROT_L=True
+    if key == glfw.KEY_D and action == glfw.PRESS:
+        ROT_R=True
+    if key == glfw.KEY_Q and action == glfw.PRESS:
+        ROT_U=True
+    if key == glfw.KEY_E and action == glfw.PRESS:
+        ROT_D=True
+    if key == glfw.KEY_K and action == glfw.PRESS:
+        ZOOM=True
+    if key == glfw.KEY_L and action == glfw.PRESS:
+        UNZOOM=True
+    if key == glfw.KEY_A and action == glfw.RELEASE:
+        ROT_L=False
+    if key == glfw.KEY_D and action == glfw.RELEASE:
+        ROT_R=False
+    if key == glfw.KEY_Q and action == glfw.RELEASE:
+        ROT_U=False
+    if key == glfw.KEY_E and action == glfw.RELEASE:
+        ROT_D=False
+    if key == glfw.KEY_K and action == glfw.RELEASE:
+        ZOOM=False
+    if key == glfw.KEY_L and action == glfw.RELEASE:
+        UNZOOM=False
+
+
+
+ROT_R = False
+ROT_L = False
+ROT_U = False
+ROT_D = False
+ZOOM = False
+UNZOOM = False 
+
+ZOOM_STEP = 0.05
+ROT_STEP = glm.radians(1.0)
 
 if __name__=="__main__":
 
@@ -64,15 +119,22 @@ if __name__=="__main__":
     glfw.set_window_pos(window, 400, 200)
     glfw.make_context_current(window)
     glfw.set_input_mode(window, glfw.STICKY_KEYS, True)
+    glfw.set_key_callback(window,key_callback)
 
     # compile shaders
     vertexShader = glCreateShader(GL_VERTEX_SHADER)
     glShaderSource(vertexShader, vertexShaderSource)
     glCompileShader(vertexShader)
+    if glGetShaderiv(vertexShader, GL_COMPILE_STATUS) == GL_FALSE:
+        print("Vertex Shader Error:")
+        print(glGetShaderInfoLog(vertexShader).decode())
 
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
     glShaderSource(fragmentShader, fragmentShaderSource)
     glCompileShader(fragmentShader)
+    if glGetShaderiv(fragmentShader, GL_COMPILE_STATUS) == GL_FALSE:
+        print("Fragment Shader Error:")
+        print(glGetShaderInfoLog(fragmentShader).decode())
 
     shaderProgram = glCreateProgram()
     glAttachShader(shaderProgram,vertexShader)
@@ -90,6 +152,8 @@ if __name__=="__main__":
     glUseProgram(shaderProgram)
     
     colors = np.random.random(size=GRID_SIZE)
+    colors = colors.astype(dtype=np.float32)
+    colors = colors.flatten()
     
     vertices = np.array([
         #bottom
@@ -153,7 +217,7 @@ if __name__=="__main__":
     glEnableVertexAttribArray(0)
     
     m = glm.identity(glm.mat4)
-    m = glm.rotate(glm.radians(45.0),glm.vec3(0,1,0)) * glm.scale(glm.vec3(0.2,0.2,0.2)) * m
+    m = glm.scale(glm.vec3(0.1,0.1,0.1)) * m
     v = glm.lookAt(glm.vec3(0.0, 0.0, 2.0), glm.vec3(0.0, 0.0, 0.0), glm.vec3(0.0, 1.0, 0.0))
     p = glm.perspective(glm.radians(60.0), WINDOW_WIDTH/WINDOW_HEIGHT, 0.1, 100.0)
     
@@ -176,11 +240,24 @@ if __name__=="__main__":
     grid_size = glGetUniformLocation(shaderProgram, "grid_size")
     glUniform3f(grid_size, GRID_SIZE[0], GRID_SIZE[1], GRID_SIZE[2])
 
+    color_data = glGetUniformLocation(shaderProgram, "color_data")
+    glUniform1fv(color_data,colors.size, colors.ctypes.data_as(ctypes.POINTER(ctypes.c_float)))
+
+
 
     def render():
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         glBindBuffer(GL_ARRAY_BUFFER,vbo)
         glBindVertexArray(vao)
+
+
+
+
+
+
+
+        glU
+
         glDrawArraysInstanced(GL_TRIANGLES, 0, 6*2*3,np.prod(GRID_SIZE))
         glfw.swap_buffers(window)
 
