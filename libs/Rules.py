2def2eea04e62cc4cc56a9fd462c191bdc5c4aad
Jakub Wojciechowski <wojciechowskicuba@gmail.com>
2026-06-17 11:47:10 +0200

    finished naive diffusion

--- libs/Rules.py
+++ libs/Rules.py
@@ -166,7 +166,7 @@ class GutDrift(Rule):
 class Diffusion(Rule):
     '''Random walk diffusion based on "Quantitative Cellular Automaton Model For Biofilms" by Pizarro G.'''
     
-    def __init__(self, geometry, a, p, target_key='substrate'):
+    def __init__(self, geometry : Geometry, a : float, p : float, target_key : str ='substrate'):
         super().__init__(geometry)
         
         self.required_keys = [f'{target_key}{x}' for x in range(6)]
@@ -180,10 +180,10 @@ class Diffusion(Rule):
         
     def apply_state(self, state) -> State:
     
-        old_layers = np.empty(shape=state.shape+(6,))
+        layers = np.empty(shape=state.shape+(6,))
         for i,key in enumerate(self.required_keys):
-            old_layers[...,i] = state[key]
-        new_layers = np.zeros_like(old_layers)
+            layers[...,i] = state[key]
+        # layers = np.zeros_like(layers)
         
         #each layer corresponds to the dircetion a particle is moving, list p contains probablities of the next direction
         # p0 - no change, p3 - 180 deg turn, p1,p2,p4,p5 - 90 degree turns
@@ -195,24 +195,27 @@ class Diffusion(Rule):
         # l4      - -y
         # l5      - -x
         
-        rolls = np.random.choice([0,1,2,3,4,5], size = old_layers.shape, p=self.p)
+        rolls = np.random.choice([0,1,2,3,4,5], size = layers.shape, p=self.p)
         
         h = np.stack([np.full(shape=state.shape, fill_value=x,dtype=np.uint8) for x in range(6)], axis=-1)
         
         rolls = ((rolls + h) %6)
         #set empty cells as invalid value
-        rolls[old_layers==0]=6
+        rolls[layers==0]=6
+        
+        #reset layers to zeros
+        layers[...]=0
         
         #does check for collisions
         for i in range(6):
             r2 = np.zeros_like(rolls)
             r2[rolls==i]=1
-            new_layers[...,i]=np.add.reduce(r2,axis=-1)
+            layers[...,i]=np.add.reduce(r2,axis=-1)
             
             
         # #doesnt check for collisions, some particles disappear
         # for i in range(6):
-        #     new_layers[np.any(rolls==i,axis=-1),i]=1
+        #     layers[np.any(rolls==i,axis=-1),i]=1
 
 
         #move particles according to their movement direction (corresponding layer)
@@ -230,25 +233,33 @@ class Diffusion(Rule):
                 sel = [slice(None)]*3
                 #save the edge next to wall and zero it in the array so that when it rolls later zeros come out on the other side
                 sel[ax] = -1
-                edge = new_layers[tuple(sel)+(i,)]
-                new_layers[tuple(sel)+(i,)] = 0
+                edge = layers[tuple(sel)+(i,)].copy()
+                layers[tuple(sel)+(i,)] = 0
                 # put the edge values on -2 as if they bounced
                 sel[ax] = -2
-                new_layers[tuple(sel)+(i,)] += edge
+                layers[tuple(sel)+(i,)] += edge
                 #similarly for the opposite direction
                 sel[ax] = 0
-                edge = new_layers[tuple(sel)+(i+3,)]
-                new_layers[tuple(sel)+(i+3,)] = 0
+                edge = layers[tuple(sel)+(i+3,)].copy()
+                layers[tuple(sel)+(i+3,)] = 0
                 # put the edge values on 1 as if they bounced
                 sel[ax] = 1
-                new_layers[tuple(sel)+(i+3,)] += edge
+                layers[tuple(sel)+(i+3,)] += edge
 
 
 
-            new_layers[...,i]=np.roll(new_layers[...,i],dir,ax)
-            new_layers[...,i+3]=np.roll(new_layers[...,i+3],dir*-1,ax)
+            layers[...,i]=np.roll(layers[...,i],dir,ax)
+            layers[...,i+3]=np.roll(layers[...,i+3],dir*-1,ax)
             #TODO: deal with collsions >1 values in arrays need to be spread out or some shit idk
             
+            
+        # naive collision resolution -> delete colliding particles
+        layers[layers>1]=1
+        
+        for i,key in enumerate(self.required_keys):
+            state[key] = layers[...,i]
+        return state
+                        
 
         
         
