f01fa261984d6ac6e9a073b0d2f942039a8edf80
jakwojc <wojciechowskicuba@gmail.com>
2026-07-15 09:34:29 +0200

    changed decay to process multiple keys

--- libs/rules/BacteriaDecay.py
+++ libs/rules/BacteriaDecay.py
@@ -4,23 +4,24 @@ from libs.Geometry import Geometry
 import numpy as np
 
 class BacteriaDecay(Rule):
-    def __init__(self, geometry:Geometry, target_key, decay_probability):
+    def __init__(self, geometry:Geometry, target_keys, decay_probability):
         super().__init__(geometry)
-        self.target_key = target_key
+        self.target_keys = target_keys
         self.decay_probability = decay_probability
         self.buff = np.zeros(geometry.size, dtype=np.float32)
 
     def apply_state(self, state:State):
 
-        grid = state[self.target_key].copy()
+        for key in self.target_keys:
+            grid = state[key].copy()
 
-        self.buff = np.random.random(size=grid.shape)
+            self.buff = np.random.random(size=grid.shape)
 
-        self.buff = grid.astype(np.bool) & (self.buff<self.decay_probability)
+            self.buff = grid.astype(np.bool) & (self.buff<self.decay_probability)
 
-        grid[self.buff==1] = 0
+            grid[self.buff==1] = 0
 
-        state[self.target_key]=grid
+            state[key]=grid
         return state
 
 
