c43f56e572b790293b5bdf6f319d0403566128a7
Maru <marustalmach@student.agh.edu.pl>
2026-07-19 11:47:49 +0200

    added Rule classes and updated Geometry class to handle neighbourhood config

---
+++ libs/neighbourhoods/MooresNeighbourhood.py
@@ -0,0 +1,14 @@
+import numpy as np
+from itertools import product
+
+class MooreNeighbourhood:
+    def generate_offsets(self, ndim: int, radius:int=1) -> np.ndarray:
+        assert ndim > 0, "dimensionality must be greater than 0"
+        assert radius > 0, "radius must be greater than 0"
+        
+        offsets = [
+            offset for offset in product((-radius, 0, radius), repeat=ndim)
+            if any(component != 0 for component in offset)
+        ]
+        return np.array(offsets, dtype=np.int64)
+    
---
+++ libs/neighbourhoods/VonNeumannsNeighbourhood.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class VonNeumannNeighborhood:
+    def generate_offsets(self, ndim: int, radius:int=1) -> np.ndarray:
+        assert ndim > 0, "dimensionality must be greater than 0"
+        assert radius > 0, "radius must be greater than 0"
+            
+        offsets = []
+        for dim in range(ndim):
+            for direction in (-radius, radius):
+                offset = [0] * ndim
+                offset[dim] = direction
+                offsets.append(tuple(offset))
+        return np.array(offsets, dtype=np.int64)
